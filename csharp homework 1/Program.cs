d0641b4e190188b50df4a5352d621b42e913935a
Andrei Mossakovski <andreimoss@gmail.com>
2022-09-05 14:02:14 +0300

    Added second homework, tasks 10,13,15

--- csharp homework 1/Program.cs
+++ csharp homework 1/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Введите, пожалуйста, номер задачи. Возможные номера: 2,4,6,8");
+﻿Console.WriteLine("Введите, пожалуйста, номер задачи. Возможные номера: 2,4,6,8 (первый семинар),10,13,15 (второй семинар)");
 string sNum = Console.ReadLine();
 
 if (sNum == "2")
@@ -38,4 +38,54 @@ if (sNum == "2")
         if (start%2 == 0) Console.WriteLine(start);
         start++;
     }
+} else if (sNum == "10") {
+    void ShowSecondDigit(int num) 
+    {
+        if (num > 999 || num < 100) 
+        {
+            Console.WriteLine("Нужно ввести именно трехзначное целое число.");
+        } else {
+            Console.WriteLine($"Вторая цифра вашего числа = {num / 10 % 10}");
+        }
+    }
+    Console.WriteLine("Введите, пожалуйста, трехзначное число.");
+    int n = Int32.Parse(Console.ReadLine());
+    ShowSecondDigit(n);
+} else if (sNum == "13") {
+    void ShowThirdDigit(int num) 
+    {
+        if (num < 100) 
+        {
+            //Если число двузначное и меньше, значит в нем третьей цифры нет
+            Console.WriteLine("В вашем числе нет третьей цифры.");
+        } else {
+            //количество цифр в числе
+            int totalDigits = num.ToString().Length;
+            //число 10 в 
+            //получаем отстаток от деления числа на 10 в степени равной количеству цифр в числе минус 2,
+            //и делим этот остаток на 10 в степени, равно количеству цифр в числе минус 3.
+            int res = num % Convert.ToInt32(Math.Pow(10,totalDigits-2));
+            int thirdNum = res / Convert.ToInt32(Math.Pow(10,totalDigits-3));
+            Console.WriteLine($"Третья цифра вашего числа = {thirdNum}");
+            //очень неудобно, что в C# нет оператора возведения в степень
+            //легкий способ был бы такой: Int32.Parse(num.ToString()[2]), но не в рамках того, что было на семинаре
+        }
+    }
+    Console.WriteLine("Введите, пожалуйста, любое целое число.");
+    int n = Int32.Parse(Console.ReadLine());
+    ShowThirdDigit(n);
+} else if(sNum == "15") {
+    void IsDayOff(int dayNum)
+    {
+        if (dayNum == 6 || dayNum == 7)
+        {
+            Console.WriteLine("Сегодня выходной!");
+        } else {
+            Console.WriteLine("Ничем не могу порадовать.");
+        }
+    }
+    Console.WriteLine("Введите, пожалуйста, номер сегодняшнего дня");
+    int d = Int32.Parse(Console.ReadLine());
+    if (d < 1 || d > 7) Console.WriteLine("Расматриваем стандартную земную неделю");
+    IsDayOff(d);
 }
